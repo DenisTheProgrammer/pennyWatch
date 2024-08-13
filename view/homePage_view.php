b455eb0e1f63f61b9924eecdeaa4094ff08cfbe6
DenisTheProgrammer <denisnic801@gmail.com>
2024-08-13 18:34:56 +0100

    Centred all elements on the page, just need to add finishing touches such as font manipulation and making buttons display side by side

--- view/homePage_view.php
+++ view/homePage_view.php
@@ -5,22 +5,24 @@
     </head>
 
     <body>
-        <div id = "logo">
-            <p> LOGO GOES HERE</p>
-        </div>
+        <div class = "frontPage">
+            <div id = "logo">
+                <p> LOGO GOES HERE</p>
+            </div>
 
-        <div id = "slogan">
-            <p> Welcome to Penny Watch, the place where every penny counts and every goal is within reach!</p>
-        </div>
+            <div id = "slogan">
+                <p> Welcome to Penny Watch, the place where every penny counts and every goal is within reach!</p>
+            </div>
 
-        <div id = "bottomButtons">
-            <form method = "post" action = "../controller/homePage_controller.php">
-                <input type = "submit" value = "Log In" name = "logIn" class = "button">
-            </form>
+            <div id = "bottomButtons">
+                <form method = "post" action = "../controller/homePage_controller.php">
+                    <input type = "submit" value = "Log In" name = "logIn" class = "button">
+                </form>
 
-            <form method = "post" action = "../controller/homePage_controller.php">
-                <input type = "submit" value = "Sign Up" name = "signUp" class = "button">
-            </form>
+                <form method = "post" action = "../controller/homePage_controller.php">
+                    <input type = "submit" value = "Sign Up" name = "signUp" class = "button">
+                </form>
+            </div>
         </div>
     </body>
 </html>
