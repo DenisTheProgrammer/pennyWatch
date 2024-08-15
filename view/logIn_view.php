1054f78027fa9665d42a5cabdcab3b7679dd950e
DenisTheProgrammer <denisnic801@gmail.com>
2024-08-15 16:02:41 +0100

    added all elements on the log in page and styled it

--- view/logIn_view.php
+++ view/logIn_view.php
@@ -2,9 +2,26 @@
 <html>
     <head>
         <link rel = "stylesheet" type = "text/css" href = "../css/main.css">
+        <link rel = "stylesheet" type = "text/css" href = "../css/logIn_style.css">
     </head>
 
     <body>
-        <p>This is where the log in page will be</p>
+        <div class = "form">
+            <div id = "formTitle">
+                <h1>Log In</h1>
+            </div>
+            <div id = "details">
+                <form method = "post" action="logIn_controller.php">
+                    <label for="username">Username (Email)</label><br>
+                    <input type="text" id = "username" name = "username"><br><br>
+                    <label for="password">Password</label><br>
+                    <input type="password" id = "password" name = "password"><br><br>
+                    <div id = "bottomButtons">
+                        <input type= "submit" value = "Log In" name = "logIn" class = "button"><br><br>
+                        <input type="submit" value = "Register" name = "register" class = "button"><br><br>
+                    </div>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
