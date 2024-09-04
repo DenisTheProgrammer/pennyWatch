6960cd25b39393d95871d497caa7591b9e3c5f17
DenisTheProgrammer <denisnic801@gmail.com>
2024-09-04 17:41:57 +0100

    added a navigation bar to the dashboard page, design needs finishing

--- view/dashboard_view.php
+++ view/dashboard_view.php
@@ -2,8 +2,16 @@
 <html>
     <head>
         <link rel = "stylesheet" type = "text/css" href = "../css/main.css">
+        <link rel = "stylesheet" type = "text/css" href = "../css/navBar.css">
     </head>
     <body>
-        <p>this will be the dashboard page</p>
+        <div class = "navBar">
+            <a class = "active" href = "../view/dashboard_view.php">Dashboard</a>
+            <a href = "#income">Income</a>
+            <a href = "#cost"> Costs</a>
+            <form method = "post" action = "../controller/logIn_controller.php" class = "navForm">
+                <input type="submit" value = "Manage Account" name = "backButton" class = "manageButton">
+            </form><!-- purpose of this is to take to a manage account page-->
+        </div>
     </body>
 </html>
