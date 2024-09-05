6b9d5927d123365f0f709d6698b5f9a49fdf7107
DenisTheProgrammer <denisnic801@gmail.com>
2024-09-05 16:12:48 +0100

    finished adding an image and styling the manage account button

--- view/dashboard_view.php
+++ view/dashboard_view.php
@@ -10,7 +10,13 @@
             <a href = "#income">Income</a>
             <a href = "#cost"> Costs</a>
             <form method = "post" action = "../controller/logIn_controller.php" class = "navForm">
-                <input type="submit" value = "Manage Account" name = "backButton" class = "manageButton">
+
+            <button type="submit" class="manageButton">
+                <img src="../images/profile.png" alt="Profile Icon" class="buttonIcon">
+                Manage Account
+            </button>
+
+                <!--<input type="submit" img src = "../images/profile.png" value = "Manage Account" name = "backButton" class = "manageButton">-->
             </form><!-- purpose of this is to take to a manage account page-->
         </div>
     </body>
