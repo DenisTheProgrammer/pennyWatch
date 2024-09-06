db7eef703e2d9b2b85ddc0f635c6b793c205e178
DenisTheProgrammer <denisnic801@gmail.com>
2024-09-06 18:27:46 +0100

    started working on the front end of the manage account page

---
+++ javascript/manageAccount_script.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("signInPopUp").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("signInPopUp").style.display = "none";
+  }
--- view/manageAccount_view.php
+++ view/manageAccount_view.php
@@ -2,9 +2,48 @@
 <html>
     <head>
         <link rel = "stylesheet" type = "text/css" href = "../css/main.css">
+        <link rel = "stylesheet" type = "text/css" href = "../css/manageAccount_style.css">
+        <link rel = "stylesheet" type = "text/css" href = "../css/logInsignUpForm_style.css">
+        <script src = "../javascript/manageAccount_script.js"></script>
     </head>
 
     <body>
-        <p>This will be the manage account page</p>
+        <div class = "topSection">
+            <form method = "post" action = "../controller/logIn_controller.php">
+                <button type="submit" class="backButton" name = "backButton">
+                    <img src = "../images/backButton.png" id = "backImage" alt = "back button">
+                </button>
+            </form>
+
+            <h1 id = "title">Manage Account</h1>
+        </div>
+
+        <div class = "forms">
+            <button class="openSignIn" onclick="openForm()">Modify Sign In</button>
+
+            <div class="signInPopUp" id="signInPopUp">
+                <div class = "form">
+                    <div id = "formTitle">
+                        <h1>Modify Sign In</h1>
+                    </div>
+                    <div id = "details">
+                        <form method = "post" action="../controller/logIn_controller.php" class="signInDetails">
+                            <label for="username"><b>Username</b></label>
+                            <input type="text" placeholder="Enter Email" name="username">
+
+                            <label for="password"><b>Password</b></label>
+                            <input type="password" placeholder="Enter Password" name="password">
+                            <div id = "bottomButtons">
+                                <button type="submit" class="confirmSignIn">Confirm Change</button>
+                                <button type="button" class="cancelButton" onclick="closeForm()">Close</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+            <button class="openDetails" onclick="openDetails()">Modify Details</button>
+        </div>
     </body>
 </html>
