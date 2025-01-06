4e24afaf7f104ead7d53f48c6452143eddbcc8d4
DenisTheProgrammer <denisnic801@gmail.com>
2025-01-06 15:35:24 +0000

    Removed an api call duplication for improved performance

--- javascript/graphs_script.js
+++ javascript/graphs_script.js
@@ -1,75 +1,63 @@
-window.onload = function () {
-    dashboardPieChart();
-    dashboardColumnChart();
-}
-
-async function dashboardPieChart() {
+window.onload = async function () {
     try {
-        // Fetch data from the server
+        // Fetch data
         const response = await fetch('../controller/graphsData_controller.php');
-        
-        // Parse the JSON response
         const data = await response.json();
-        
-        const chart = new CanvasJS.Chart("pieChartContainer", {
-            backgroundColor: "transparent",
-            animationEnabled: true,
-            title: {
-                text: "Incomes vs Costs",
-                fontColor: "white"
-            },
-            subtitles: [{
-                text: "this month's breakdown",
-                fontColor: "white"
-            }],
-            data: [{
-                type: "pie",
-                indexLabel: "{label}: {y}",
-                indexLabelFontColor: "white",
-                dataPoints: data.dashboardPieChart
-            }]
-        });
 
-        chart.render();
+        dashboardPieChart(data.dashboardPieChart);
+        dashboardColumnChart(data.dashboardColumnChart);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+};
+
+function dashboardPieChart(dashboardPieData) {
+    const chart = new CanvasJS.Chart("pieChartContainer", {
+        backgroundColor: "transparent",
+        animationEnabled: true,
+        title: {
+            text: "Incomes vs Costs",
+            fontColor: "white"
+        },
+        subtitles: [{
+            text: "this month's breakdown",
+            fontColor: "white"
+        }],
+        data: [{
+            type: "pie",
+            indexLabel: "{label}: {y}",
+            indexLabelFontColor: "white",
+            dataPoints: dashboardPieData
+        }]
+    });
+
+    chart.render();
 }
 
+function dashboardColumnChart(dashboardColumnData) {
+    const chart = new CanvasJS.Chart("columnChartContainer", {
+        backgroundColor: "transparent",
+        animationEnabled: true,
+        theme: "light2",
+        title: {
+            text: "Monthly Disposable Income",
+            fontColor: "white"
+        },
+        axisX: {
+            labelFontColor: "white"
+        },
+        axisY: {
+            title: "Disposable Income (£)",
+            titleFontColor: "white",
+            labelFontColor: "white"
+        },
+        data: [{
+            type: "column",
+            yValueFormatString: "#,##0.## £",
+            dataPoints: dashboardColumnData
+        }]
+    });
 
-async function dashboardColumnChart() {
-    try {
-        // Fetch data from the server
-        const response = await fetch('../controller/graphsData_controller.php');
-        
-        // Parse the JSON response
-        const data = await response.json();
-        
-        const chart = new CanvasJS.Chart("columnChartContainer", {
-            backgroundColor: "transparent",
-            animationEnabled: true,
-            theme: "light2",
-            title: {
-                text: "Monthly Disposable Income",
-                fontColor: "white"
-            },
-            axisX: {
-                labelFontColor: "white"
-            },
-            axisY: {
-                title: "Disposable Income (£)",
-                titleFontColor: "white",
-                labelFontColor: "white"
-            },
-            data: [{
-                type: "column",
-                yValueFormatString: "#,##0.## £",
-                dataPoints: data.dashboardColumnChart
-            }]
-        });
-
-        chart.render();
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+    chart.render();
 }
+
