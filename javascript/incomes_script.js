fac4c6e4b5b63b8edb6c8f658c196ee6c0973309
DenisTheProgrammer <denisnic801@gmail.com>
2025-01-29 10:58:31 +0000

    further minor changes for fitting large sets of data

--- javascript/incomes_script.js
+++ javascript/incomes_script.js
@@ -1,6 +1,6 @@
 import { pagination } from "./pagination.js";
 
-document.addEventListener("DOMContentLoaded", () => pagination(7, "incomeTable"));
+document.addEventListener("DOMContentLoaded", () => pagination(10, "incomeTable"));
 function filterDisplay()
 {
     const form = document.getElementById('filterForm');
